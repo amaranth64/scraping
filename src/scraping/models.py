6c5a3c4b5c96367d6a01ae648c65193e926acc0e
amaranth64 <amaranth64@yandex.ru>
2021-01-04 22:16:05 +0400

    first commit 1

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name="Название")
     slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Города'
+        verbose_name_plural = "Города"
+        
+    def __str__(self):
+        return self.name
