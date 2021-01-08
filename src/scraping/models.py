23fbf91ce3d28a8dac80ee1b003c6ba586c30302
amaranth64 <amaranth64@yandex.ru>
2021-01-08 15:10:52 +0400

    Add vacations

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,13 +1,56 @@
 from django.db import models
+from django.db.models import ForeignKey
+
+from scraping.utils import from_cyrillic_to_eng
+
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Название")
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
     slug = models.CharField(max_length=50, blank=True)
 
     class Meta:
-        verbose_name = 'Города'
+        verbose_name = 'Город'
         verbose_name_plural = "Города"
-        
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(self.name)
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", unique=True)
+    slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = "Языки программирования"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(self.name)
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name="Название")
+    company = models.CharField(max_length=250, verbose_name="Компания")
+    description = models.TextField(verbose_name="Описание")
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name="Город")
+    language = models.ForeignKey("Language", on_delete=models.CASCADE, verbose_name="Язык программированияу")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = "Вакансии"
+
+    def __str__(self):
+        return self.title
