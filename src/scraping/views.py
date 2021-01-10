4d966a8af56b22233f9b46c36610fd32eff9d195
amaranth64 <amaranth64@yandex.ru>
2021-01-10 22:26:17 +0400

    Bootstrap

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Vacancy
+
+
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, "scraping/home.html", {"object_list": qs})
+
 # Create your views here.
